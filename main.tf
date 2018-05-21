6f092f13d3f07d9e7d79c1410494f17e3128ed8c
Dan Brown <roooms@gmail.com>
2018-05-21 08:52:28 +0100

    Switch to terraform-aws-modules/vpc/aws

--- main.tf
+++ main.tf
@@ -2,32 +2,37 @@ variable "region" {
   description = "AWS region to use"
 }
 
-variable "config_name" {
-  description = "Unique configuration name"
+variable "namespace" {
+  description = "Namespace for configuration"
 }
 
 provider "aws" {
   region = "${var.region}"
 }
 
-module "simple_network" {
-  source  = "dan-ptfe.hashidemos.io/bank/simple_network/aws"
-  version = "0.2.1"
-  
-  config_name = "${var.config_name}"
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "1.31.0"
+
+  name = "${var.namespace}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["${data.aws_availability_zones.available.names[0]}"]
+  public_subnets = ["10.0.1.0/24"]
+
+  tags = {
+    Namespace = "${var.namespace}"
+  }
 }
 
 output "vpc_id" {
   description = "VPC ID"
-  value       = "${module.simple_network.vpc_id}"
+  value       = "${module.vpc.vpc_id}"
 }
 
-output "subnet_id" {
-  description = "Subnet ID"
-  value       = "${module.simple_network.subnet_id}"
-}
-
-output "availability_zone" {
-  description = "Availability Zone"
-  value       = "${module.simple_network.availability_zone}"
+output "public_subnets" {
+  description = "Public Subnet IDs"
+  value       = "${module.vpc.public_subnets}"
 }
